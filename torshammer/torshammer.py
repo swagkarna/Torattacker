bc6e8231357ee6676c87a4014efab3bf42ef5e6e
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-10-10 06:17:24 +0700

    Create torshammer.py

---
+++ torshammer/torshammer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python                                                                                                   """
+Tor's Hammer - Slow POST Denial Of Service Testing Tool
+Version 1.0 Beta                                          Project home page: https://sourceforge.net/projects/torsha                                                          Tor's Hammer is a slow post dos testing tool written in PyIt can also be run through the Tor network to be anonymizeIf you are going to run it with Tor it assumes you are runKills most unprotected web servers running Apache and IIS Kills Apache 1.X and older IIS with ~128 threads.
+Kills newer IIS and Apache 2.X with ~256 threads.
+"""
